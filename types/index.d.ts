ef55c35103c411611cb0aaf4790ce91db44939c4
Blayne Chard <blayne@chard.com>
2020-07-18 17:15:05 +1200

    build: initial commit

---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+export { STACCatalogSpecification as Catalog } from './catalog';
+export { STACCollectionSpecification as Collection } from './collection';
+export { STACItem as Item } from './item';
